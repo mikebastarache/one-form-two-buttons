89c8fb911c13ff080d9a6212c973f42bdb7a269a
Mike Bastarache <mike@digitalmagic.ca>
2013-06-19 09:23:45 -0300

    First commit
    simple jquery sample

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        //
+        // POST: /Home
+        [HttpPost]
+        public ActionResult Index(FormCollection oCollection)
+        {
+            ViewBag.Message = "Post back happend";
+
+            foreach (var key in oCollection.AllKeys)
+            {
+                var value = oCollection[key];
+                Response.Write(value + "<br>");
+            }
+
+            foreach (var key in oCollection.Keys)
+            {
+                var value = oCollection[key.ToString()];
+                Response.Write(value + "<br>");
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcApplication1/Models/HomeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class HomeView
+    {
+        [Required(ErrorMessage = "Error message here.")]
+        [RegularExpression(@"^[^,<>\s@]+@([^\s,@\.\[\]]+\.)*[^\s,@\.\[\]]+\.[^\s,@\.\[\]]+$", ErrorMessage = "Invalid Email")]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        public bool AcceptRules { get; set; }
+    }
+}
